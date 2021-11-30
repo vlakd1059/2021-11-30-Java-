c0b1f0edadc1e9a01fd7dbc24b5d7853081c623f
vlakd1059 <49181149+vlakd1059@users.noreply.github.com>
2021-11-30 09:34:34 +0900

    // java festival 문제1~2

--- Test02.java
+++ Test02.java
@@ -3,10 +3,20 @@ package 자바테스트;
 public class Test02 {
 
 	public static void main(String[] args) {
-		;
-		
-
-
+		int a = 0;
+		int b = 78;
+		int sum=0;
+		while (a < 77) {
+			a++;
+			b--;
+			sum+=a*b;
+		}
+		System.out.println(sum);
 	}
-
 }
+
+
+
+//0+1*77
+//0+1*77+2*76
+//3*75
